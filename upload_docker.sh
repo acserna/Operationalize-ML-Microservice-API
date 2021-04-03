900cd29a82b1aa70a926feb1665db7030e60543f
Andres <andres.c.serna@gmail.com>
2021-04-03 01:40:01 -0500

    fix upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpathb="andrecamilosr/mlapp"
+dockerpath="andrecamilosr/mlapp"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
 docker login --username "andrecamilosr" &&\
 	docker image tag mlapp $dockerpath
+echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
