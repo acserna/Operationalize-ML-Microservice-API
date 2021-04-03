698ad16b440cdb231bf2544fccb1452b4e43af9e
Andres <andres.c.serna@gmail.com>
2021-04-03 01:30:53 -0500

    upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpathb="andrecamilosr/mlapp"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username "andrecamilosr" &&\
+	docker image tag mlapp $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
